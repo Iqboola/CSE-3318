f1fb80a0c6eeaecface6a87443a50f9e2bee3ae9
Ali <ali.iqbal3578@gmail.com>
2025-01-23 11:06:41 -0600

    added hw1 files

---
+++ hw1_files/text_indexing.c
@@ -0,0 +1,28 @@
+/*
+ * Starter code
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+
+    char *fname_par = NULL;
+    char *fname_words = NULL;
+
+    int mode = 0; // 0 - normal, 1 - verbose
+
+    if (argc < 4 ) {
+        printf("Not enough arguments. Need mode and 2 filenames, e.g.:   0 small.txt words1.txt\n");
+        return 0;
+    }
+    else {
+        fname_par = argv[2];
+        fname_words = argv[3];
+        mode = atoi(argv[1]);
+    }
+
+    printf("mode: %d  |  paragraph: %s  |  words: %s\n", mode, fname_par, fname_words);  // KEEP THIS LINE
+
+    return 0;
+}
