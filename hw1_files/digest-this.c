ae924cd289ff470748bb7d8f701cbe3a3872a549
Ali Iqbal <ali.iqbal3578@gmail.com>
2025-01-26 18:19:51 -0600

    removed some comments

--- hw1_files/digest-this.c
+++ hw1_files/digest-this.c
@@ -86,9 +86,6 @@ int main(int argc, char *argv[])
     
     for (int i = 0; i < length; i++) 
     {
-        // %4d => right‐align integer in at least 4 spaces
-        // %20p => right‐align pointer in at least 20 spaces
-        // %-10s => left‐align string in a 10‐character field
         printf("%4d | %16p | %-10s\n", i, (void *)words[i], words[i]);
     }
 
